d6fd23a39e4519a1eddd9366b6375d921eba9800
massun-onibakuchi <etherdao0x00@gmail.com>
2022-12-06 17:12:00 +0900

    remove example contract

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -4,13 +4,6 @@ pragma solidity ^0.8.15;
 import "foundry-huff/HuffDeployer.sol";
 import "forge-std/Script.sol";
 
-interface SimpleStore {
-    function setValue(uint256) external;
-    function getValue() external returns (uint256);
-}
-
 contract Deploy is Script {
-    function run() public returns (SimpleStore simpleStore) {
-        simpleStore = SimpleStore(HuffDeployer.deploy("SimpleStore"));
-    }
+    function run() public {}
 }
