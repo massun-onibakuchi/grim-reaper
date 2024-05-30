eb07bb47b1c0654fcc410f7cc24919103aed00e5
onibakuchi <etherdao0x00@gmail.com>
2024-05-30 16:26:43 +0400

    test

--- test/GrimReaper.t.sol
+++ test/GrimReaper.t.sol
@@ -5,7 +5,7 @@ import "foundry-huff/HuffDeployer.sol";
 import "forge-std/Test.sol";
 
 import {GrimReaper} from "../src/GrimReaper.sol";
-import {OptimizedGrimReaper} from "../src/OptimizedGrimReaper.sol";
+import {OptimizedGrimReaper, OptimizedGrimReaperV2} from "../src/OptimizedGrimReaper.sol";
 import "./MockERC20.sol";
 import "./MockPool.sol";
 
@@ -15,7 +15,7 @@ abstract contract GrimReaperBaseTest is Test {
 
     GrimReaper public reaper;
 
-    MockERC20 collateral;
+    MockERC20 collateral = MockERC20(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
     MockERC20 debt;
     MockPool pool = MockPool(POOL);
 
@@ -23,7 +23,8 @@ abstract contract GrimReaperBaseTest is Test {
 
     /// @dev Setup the testing environment.
     function setUp() public virtual {
-        collateral = new MockERC20();
+        MockERC20 _token = new MockERC20();
+        vm.etch(address(collateral), address(_token).code);
         debt = new MockERC20();
         MockPool _pool = new MockPool();
         vm.etch(POOL, address(_pool).code);
@@ -36,15 +37,21 @@ abstract contract GrimReaperBaseTest is Test {
         // fund
         collateral.mint(POOL, liquidationBonus);
         debt.mint(address(reaper), 1000);
+
+        vm.label(address(collateral), "collateral");
+        vm.label(address(debt), "debt");
+        vm.label(POOL, "pool");
+        vm.label(address(reaper), "GrimReaper");
+        vm.label(address(0xbabe), "babe");
     }
 
     function _deployGrimReaper() internal virtual;
 
     function testLiquidate() public {
-        testLiquidate(1000e18, address(0xbabe));
+        testFuzz_Liquidate(1000e18, address(0xbabe));
     }
 
-    function testLiquidate(uint256 amount, address user) public {
+    function testFuzz_Liquidate(uint256 amount, address user) public {
         vm.assume(amount < type(uint128).max);
         deal(address(debt), address(reaper), amount, true);
 
@@ -122,9 +129,49 @@ contract OptimizedGrimReaperSolTest is GrimReaperBaseTest {
     function getLiquidationPayload(address _col, address _debt, address _user, uint256 _debtToCover)
         internal
         pure
+        virtual
         returns (bytes memory payload)
     {
         payload = abi.encodePacked(_col, _debt, _user, uint128(_debtToCover));
+    }
+}
+
+contract Deployer {
+    constructor(bytes memory table) {
+        bytes memory solidityCode = type(OptimizedGrimReaperV2).runtimeCode;
+        bytes memory code = bytes.concat(solidityCode, table);
+        assembly {
+            return(add(code, 32), mload(code))
+        }
+    }
+}
+
+contract OptimizedGrimReaperSolV2Test is OptimizedGrimReaperSolTest {
+    /// @dev abi.encodePacked(WETH, USDC, LUSD);
+    bytes constant COLLATERAL_ASSET_TABLE =
+        hex"C02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2A0b86991c6218b36c1d19D4a2e9Eb0cE3606eB485f98805A4E8be255a32880FDeC7F6728C6568bA0";
+
+    function _deployGrimReaper() internal override {
+        reaper = GrimReaper(address(new Deployer(COLLATERAL_ASSET_TABLE)));
+    }
+
+    function getLiquidationPayload(address _col, address _debt, address _user, uint256 _debtToCover)
+        internal
+        pure
+        override
+        returns (bytes memory payload)
+    {
+        uint8 id;
+        if (_col == 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2) {
+            id = 0;
+        } else if (_col == 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48) {
+            id = 1;
+        } else if (_col == 0x5f98805A4E8be255a32880FDeC7F6728C6568bA0) {
+            id = 2;
+        } else {
+            revert("invalid collateral");
+        }
+        payload = abi.encodePacked(_debt, uint8(id), _user, uint128(_debtToCover));
     }
 }
 
